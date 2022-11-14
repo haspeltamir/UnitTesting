740545a5025d1dd5109eaaf50c703d3c1d4de589
tamir haspel <haspeltamir@gmail.com>
2022-11-14 22:20:48 +0200

    interface

---
+++ checkhw/checkhw/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace checkhw
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Show the dialog that allows user to select a file, the 
+            // call will result a value from the DialogResult enum
+            // when the dialog is dismissed.
+            DialogResult result = this.openFileDialog1.ShowDialog();
+            // if a file is selected
+            if (result == DialogResult.OK)
+            {
+                // Set the selected file URL to the textbox
+                this.filetext.Text = this.openFileDialog1.FileName;
+            }
+        }
+
+        private void Next_btn_Click(object sender, EventArgs e)
+        {
+            var myForm = new Cp();
+            myForm.Show();
+            this.Hide();
+        }
+    }
+}
